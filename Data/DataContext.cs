13082596a7dea8f76db52a782af90b861cbf0fee
johnnydevsolutions <johnny_sap178@yahoo.com.br>
2023-01-20 14:38:46 -0300

    inicio do projeto

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
